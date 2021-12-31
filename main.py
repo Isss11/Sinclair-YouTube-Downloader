ac07e57a581de4f1c05782a7850294f353d26e67
Isss11 <89956249+Isss11@users.noreply.github.com>
2021-12-30 22:26:46 -0500

    Add files via upload
    This adds exception handling, along with progress responses based on whether the video is downloading or not.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from tkinter import *
 from tkinter import ttk
 from pytube import YouTube
+from time import sleep
+
+#NOTE this program was only created for recreational use, not for commercial purposes.
 
 """
 This program is created by Isaiah Sinclair with the use of tkinter and pytube.
@@ -8,13 +11,31 @@ This program is created by Isaiah Sinclair with the use of tkinter and pytube.
 
 #This is the function for when the button is clicked (command callback)
 def downloadVideo():
-    link = (linkEntry.get()).rstrip() #takes any trailing spaces away
+    try:
+        downloadButton.state(['disabled']) #While the video is downloading, you cannot press the button again
+        progressText.configure(text= "Video downloading. Sit tight!")
+        frame.update()
 
-    yt = YouTube(link)
+        link = (linkEntry.get()).rstrip() #takes any trailing spaces away
 
-    ytStream = yt.streams.get_by_itag(22) #chooses a stream that is MP4 and I believe 720 p -- found this selection online
+        yt = YouTube(link)
 
-    ytStream.download() #specific directory where I want my videos downloaded to
+        ytStream = yt.streams.get_by_itag(22) #chooses a stream that is MP4 and I believe 720 p -- found this selection online
+
+        ytStream.download()
+
+        progressText.configure(text= "Your video has finished downloading!")
+        frame.update()
+    except: 
+        progressText.configure(text= "Incorrect input! Make sure you are inputting a proper YouTube URL.")
+        frame.update()
+
+    sleep(2) #Added a little wait so this statement will be seen.
+    downloadButton.state(['!disabled']) #Button is reinstated
+    progressText.configure(text= "Input another YouTube video URL to download.")
+    frame.update()
+    
+
 
 #Main function below
 root = Tk() #setting up the window and frame
@@ -27,16 +48,20 @@ frame['relief'] = 'groove' #setting up how boundaries of frame looks
 frame.grid(column=0, row=0, sticky=(N, W, E, S))
 
 
-title = ttk.Label(frame, text = 'YouTube Video Downloader')
+title = ttk.Label(frame, text = 'Sinclair YouTube Downloader', font= 'Helvetica 14 bold')
 title.grid(row = 0)
 
 #creating entry object
 link = StringVar()
 linkEntry = ttk.Entry(frame, textvariable=link, width= 50)
-linkEntry.grid(row = 20)
+linkEntry.grid(row = 1)
 
 #creating button
 downloadButton = ttk.Button(frame, text='Download', command = downloadVideo)
-downloadButton.grid(row = 20, column = 40)
+downloadButton.grid(row = 1, column = 2)
+
+#Creating progess label
+progressText = ttk.Label(frame, text= "Input a YouTube video URL to download.")
+progressText.grid(row = 3)
 
 root.mainloop()
